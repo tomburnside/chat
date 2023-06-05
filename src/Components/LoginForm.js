f4f51d49654b74c01e420213537ac615a9cbf318
tomburnside <tomburnside90@gmail.com>
2023-06-05 19:20:40 +1000

    minor change

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -22,7 +22,7 @@ const Modal = () => {
       window.location.reload();
       setError('');
     } catch (err) {
-      setError('Oops, incorrect credentials.');
+      setError('Ooops, incorrect credentials.');
     }
   };
 
